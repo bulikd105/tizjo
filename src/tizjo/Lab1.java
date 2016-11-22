998118d3e3af940d69686f02f6143475f6c7f41a
Damian <Damian@DESKTOP-UM09VT4>
2016-11-22 22:12:53 +0100

    Poprawienie metody do otrzymywania danych od użytkownika, dodanie catchy i obsługa wyjątków

--- src/tizjo/Lab1.java
+++ src/tizjo/Lab1.java
@@ -1,6 +1,5 @@
 package tizjo;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -9,58 +8,73 @@ import java.util.Scanner;
 
 public class Lab1 
 {
-	static String hello = "hello world";
-	
 	public static void main(String[] args)
 	{
 		CallBackImpl callBack = new CallBackImpl();
-		String str[] = GetData();
-		SecMet(str, callBack);
+		String str[] = new String[3];
+		str = GetData();
 		
+		SecMet(str, callBack);
 	}
 	
+	// Pobieranie danych od usera
 	public static String[] GetData()
 	{
-		String str[] = new String[4];
+		String userData[] = new String[3];
 		
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("Podaj nazwe pliku");
-		str[1] = scan.nextLine();
-		System.out.println("Podaj str1");
-		str[2] = scan.nextLine();
-		System.out.println("Podaj str2");
-		str[3] = scan.nextLine();
-	
+		while(true)
+		{
+			System.out.println("Podaj nazwe pliku");
+			userData[0] = scan.nextLine();
+			if(userData[0].endsWith(".txt"))
+			{
+				break;
+			}
+			else
+			{
+				System.out.println("Podaj œcie¿kê do pliku o rozszerzeniu '.txt'");
+			}
+		}
+
+		System.out.println("Podaj 'poprawny' ciag znakow");
+		userData[1] = scan.nextLine();
 		
-		return str;
+		System.out.println("Podaj 'niepoprawny' ciag znakow");
+		userData[2] = scan.nextLine();
+	
+		return userData;
 	}
 	
 	public static void SecMet(String[] str, CallBackImpl callBack)
 	{
 		boolean flag = false;
-		try
+		String hello = "Hello World!";
+		do
 		{
-			File filee = new File(str[1]);
-			FileWriter fw = new FileWriter(filee);
-			BufferedWriter bw = new BufferedWriter(fw);
-			
-			bw.write(hello);
-			bw.close();
-			flag = true;
-			
-			System.out.println("result : " + str[2]);
-			
-		}
-		catch(IOException e)
-		{
-			System.out.println("result : " +str[3]);
-			flag = false;
-			e.printStackTrace();
-			
-		}	
+			try
+			{
+				File filee = new File(str[0]);
+				FileWriter fw = new FileWriter(filee);
+				BufferedWriter bw = new BufferedWriter(fw);
+				
+				bw.write(hello);
+				bw.close();
+				flag = true;
+				
+				System.out.println("result : " + str[1]);
+				
+			}
+			catch(IOException e)
+			{
+				System.out.println("result : " +str[2]);
+				flag = false;
+				e.printStackTrace();
+				
+			}	
+		}while(!callBack.methodToCallBack(flag).equals(""));
 		
-		callBack.methodToCallBack(flag);
 	}
 }
 
