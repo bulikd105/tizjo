587b499dc4affc76a5a1f35598f2942541814dd1
fletchPL <fletchPL@gmail.com>
2016-11-23 08:18:59 +0100

    Refactor klasy głównej. Przeniesienie drugiej metody do klasy abstrakcyjnej.

--- src/tizjo/Lab1.java
+++ src/tizjo/Lab1.java
@@ -1,12 +1,8 @@
 package tizjo;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Scanner;
 
-public class Lab1 
+public class Lab1 extends Second
 {
 	public static void main(String[] args)
 	{
@@ -45,36 +41,6 @@ public class Lab1
 		userData[2] = scan.nextLine();
 	
 		return userData;
-	}
-	
-	public static void SecMet(String[] str, CallBackImpl callBack)
-	{
-		boolean flag = false;
-		String hello = "Hello World!";
-		do
-		{
-			try
-			{
-				File filee = new File(str[0]);
-				FileWriter fw = new FileWriter(filee);
-				BufferedWriter bw = new BufferedWriter(fw);
-				
-				bw.write(hello);
-				bw.close();
-				flag = true;
-				
-				System.out.println("result : " + str[1]);
-				
-			}
-			catch(IOException e)
-			{
-				System.out.println("result : " +str[2]);
-				flag = false;
-				e.printStackTrace();
-				
-			}	
-		}while(!callBack.methodToCallBack(flag).equals(""));
-		
 	}
 }
 
---
+++ src/tizjo/Second.java
@@ -0,0 +1,39 @@
+package tizjo;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public abstract class Second 
+{
+	public static void SecMet(String[] str, CallBackImpl callBack)
+	{
+		boolean flag = false;
+		String hello = "Hello World!";
+		int iterator = 0;
+		do
+		{
+			try
+			{
+				File filee = new File(str[0]);
+				FileWriter fw = new FileWriter(filee);
+				BufferedWriter bw = new BufferedWriter(fw);
+				
+				bw.write(hello);
+				bw.close();
+				flag = true;
+				
+				System.out.println("result : " + str[1]);
+				iterator++;
+			}
+			catch(IOException e)
+			{
+				System.out.println("result : " +str[2]);
+				flag = false;
+				e.printStackTrace();
+			}		
+		}
+		while(!callBack.methodToCallBack(flag).equals("") && iterator < 1);
+	}
+}
